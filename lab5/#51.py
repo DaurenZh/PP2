bf94657456e4782ea5a47f15a2c0c2ee0c5d0608
Dauren <zhylkybaevdauren@gmail.com>
2024-02-19 19:05:26 +0600

    lab5

---
+++ lab5/#51.py
@@ -0,0 +1,12 @@
+import re
+
+def match_pattern(string):
+    pattern = r'ab*'
+    if re.fullmatch(pattern, string):
+        return True
+    else:
+        return False
+
+test_strings = ["ab", "aab", "abb", "aabb", "abbbbb", "ac", "abc", "cba"]
+for test_string in test_strings:
+    print(f"{test_string}: {match_pattern(test_string)}")
